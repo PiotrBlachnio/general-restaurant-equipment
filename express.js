4d757a43e55e6ffe7f7da12ef84c967fcdc4e058
Piotr Blachnio <piotr.blachnio@yahoo.com>
2024-04-23 14:28:25 +0700

    Update express.js

--- express.js
+++ express.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -6,9 +8,17 @@ const portNumber = 5016;
 
 const sourceDir = "dist";
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(sourceDir));
 
 app.use(express.static("public"));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(portNumber, () => {
     console.log(`Server started on http://localhost:${portNumber}`);
